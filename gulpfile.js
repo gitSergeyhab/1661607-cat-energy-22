0be8788c237efb8d71180f759b869395566856b6
Sergey <redmoloch@yandex.ru>
2021-04-01 21:29:00 +0300

    remade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,36 @@
-const gulp = require("gulp");
+// const gulp = require("gulp");
+const {src, dest, series, watch} = require('gulp');
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const includer = require('gulp-file-include');
+const beautify = require('gulp-beautify').html;
+
+const htmlProto = (fileName) => {
+  return src(`source/html/${fileName}.html`)
+    .pipe(includer({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(beautify({
+      end_with_newline: true,
+      indent_size: 2
+    }))
+    .pipe(dest('source'))
+    .pipe(sync.stream())
+}
+
+const indexHtml = () => htmlProto('index');
+const catalogHtml = () => htmlProto('catalog');
+const formHtml = () => htmlProto('form');
 
 // Styles
 
 const styles = () => {
-  return gulp.src("source/sass/style.scss")
+  return src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
@@ -17,7 +38,7 @@ const styles = () => {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(dest("source/css"))
     .pipe(sync.stream());
 }
 
@@ -42,10 +63,14 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  // gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  // gulp.watch("source/*.html").on("change", sync.reload);
+  watch("source/sass/**/*.scss", styles);
+  watch('source/html/**/*.html', indexHtml);
+  watch('source/html/**/*.html', catalogHtml)
+  watch('source/html/**/*.html', formHtml)
+  
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+
+exports.default = series(indexHtml, catalogHtml, formHtml, styles, server, watcher);
