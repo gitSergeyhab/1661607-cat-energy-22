6632938306c6dfc3f6cb262b5edff9c2d3a3baa6
Sergey <redmoloch@yanadex.ru>
2021-04-03 21:34:13 +0300

    remade header-menu for @media tablet

--- source/js/script.js
+++ source/js/script.js
@@ -36,7 +36,18 @@ const menuBtn = document.querySelector('.page-header__nav-toggle');
 const spanBtn = menuBtn.querySelector('span');
 const menuList = document.querySelector('.site-list');
 
-menuList.style.display = 'none';
+if (window.innerWidth < 768) {
+  menuList.style.display = 'none';
+}
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    menuList.style.display = 'none';
+  } else {
+    menuList.style.display = 'flex';
+  }
+})
+
 spanBtn.classList.add('page-header__nav-open');
 
 menuBtn.addEventListener('click', () => {
